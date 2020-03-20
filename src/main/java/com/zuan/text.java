2a496bf7612480d0fd9026d923a445455e821f5c
azuan <15028026328@163.com>
2020-03-20 17:07:22 +0800

    ---A 这是我第san次提交的代码

--- src/main/java/com/zuan/text.java
+++ src/main/java/com/zuan/text.java
@@ -5,6 +5,7 @@ public class text {
     public static void main(String[] args) {
         System.out.println("11212");
         System.out.println("232323");
+        System.out.println("2323233333");
     }
 
 }
